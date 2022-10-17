3b4851867a14ad1f4005301252e7fb17fb9cca1f
LuPinheiro <gerallupinheiro@gmail.com>
2022-10-17 09:55:04 +0200

    Python Exercices

---
+++ Hola Mundo/Exercises.py
@@ -0,0 +1,56 @@
+
+
+#############################
+###  Exercise Hola mundo  ###
+#############################
+
+# Exercise number 1
+print('Ejercicio 1')
+print('Hola Mundo')
+
+# Exercise number 2
+print('Ejercicio 2')
+saludos = 'Mundo'
+print('Hola ' + str(saludos))
+
+
+# Exercise number 3
+print('Ejercicio 3')
+print('Hola')
+print('Mundo')
+
+# Exercise number 4
+print('Ejercicio 4')
+print('Hola', 'Mundo')
+
+# Exercise number 5
+print('Ejercicio 5')
+saludos ='Mundo'
+print('Hola', saludos)
+
+# Exercise number 6
+print('Ejercicio 6')
+print('Hola ', end='')
+print('Mundo')
+
+
+# Exercise number 7
+print('Ejercicio 7')
+m ='Mmundo'
+print('Hola',f'{m}')
+
+
+# Exercise number 8
+print('Ejercicio 8')
+saludos = 'Mundo'
+print(f'Hola {saludos}')
+
+# Exercise number 9
+print('Ejercicio 9')
+
+def hola():
+    saludos= 'Hola Mundo'
+    print(saludos)
+    return
+
+hola()
